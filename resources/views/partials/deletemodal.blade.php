33fc9fd107485f004bb562a89c8e97809411598d
raffaelecapaldo <raffaelecpl@gmail.com>
2023-05-30 15:04:28 +0200

    Translated modal

--- resources/views/partials/deletemodal.blade.php
+++ resources/views/partials/deletemodal.blade.php
@@ -3,15 +3,15 @@
     <div class="modal-dialog">
         <div class="modal-content">
             <div class="modal-header">
-                <h1 class="modal-title fs-5" id="deleteModalLabel">ATTENZIONE</h1>
+                <h1 class="modal-title fs-5" id="deleteModalLabel">WARNING</h1>
                 <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
             <div class="modal-body">
-                <p>Confermi di voler eliminare <span id="modal-item-title"></span>?</p>
+                <p>Are you sure you want to delete the comics: <span class="fw-bold" id="modal-item-title"></span>?</p>
             </div>
             <div class="modal-footer">
-                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Chiudi</button>
-                <button type="button" class="btn btn-primary">Elimina</button>
+                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                <button type="button" class="btn btn-primary">Delete</button>
             </div>
         </div>
     </div>
