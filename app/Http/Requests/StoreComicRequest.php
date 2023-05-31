499d2ceaf29046c21787b14ae2536ee0d5961d7f
raffaelecapaldo <raffaelecpl@gmail.com>
2023-05-31 15:32:43 +0200

    Added italian error messages (BONUS)

--- app/Http/Requests/StoreComicRequest.php
+++ app/Http/Requests/StoreComicRequest.php
@@ -36,4 +36,23 @@ class StoreComicRequest extends FormRequest
 
         ];
     }
+
+//Errori in italiano per il bonus
+    public function messages()
+    {
+        return [
+            'title.required' => 'Il titolo è obbligatorio',
+            'title.min' => 'Il titolo deve essere di almeno 3 caratteri',
+            'title max' => 'Il titolo non può essere più lungo di 255 caratteri',
+            'thumb.required' => 'Un URL è obbligatorio',
+            'thumb.url' => 'URL non corretto, riprovare',
+            'sale_date.required' => 'La data di messa in vendita è obbligatoria',
+            'sale_date.date' => 'La data è errata, controllare il formato',
+            'writers.required' => 'Almeno un autore è obbligatorio',
+            'series.required' => 'La serie a cui il fumetto appartiene è un campo obbligatorio',
+            'price.required' => 'Il campo prezzo è obbligatorio',
+            'price.decimal' => 'Prezzo nel formato errato, inserire una cifra con due decimali',
+            'description.required' => 'La descrizione del fumetto è obbligatoria',
+        ];
+    }
 }
